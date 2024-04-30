e349cb11fd27e9e3293acb715738edf3d103d2fa
Ella Järvinen <eltsu.jarvinen@gmail.com>
2024-04-30 13:27:22 +0300

    muunnoksianam

--- js-rocket/js/rocket.js
+++ js-rocket/js/rocket.js
@@ -6,22 +6,27 @@ var changeState = function (state) {
     document.body.className = 'body-state'+
     state;
     clearInterval(timer)
-    
+    var countdownNumber= 10;
+    document.getElementById('countdown').
+    innerHTML = countdownNumber;
+
     if(state == 2) {
         timer = setInterval(function () {
-            document.getElementById('countdown').
-            innerHTML = countdownNumber;
             countdownNumber = countdownNumber
             -1;
+            document.getElementById('countdown').
+            innerHTML = countdownNumber;
             if (countdownNumber <= 0) {
                 changeState(3);
             };
-        }, 1000);
+        }, 500);
     } else if (state == 3) {
         var success = setTimeout(function ()
         {
             var randomNumber = Math.round
                 (Math.random()*10);
+            
+            console.log('randomNumber:', randomNumber)
                 
                 //success
                 if (randomNumber > 5) {
