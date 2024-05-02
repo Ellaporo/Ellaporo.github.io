d40cd3ee35be69d5a5c8e77359c5ac0ceece9541
Ella Järvinen <eltsu.jarvinen@gmail.com>
2024-05-02 11:21:24 +0300

    RAKETTI

--- js-rocket/js/rocket.js
+++ js-rocket/js/rocket.js
@@ -10,12 +10,28 @@ var changeState = function (state) {
     document.getElementById('countdown').
     innerHTML = countdownNumber;
 
+    // countdown
     if(state == 2) {
         timer = setInterval(function () {
-            countdownNumber = countdownNumber
-            -1;
-            document.getElementById('countdown').
-            innerHTML = countdownNumber;
+            countdownNumber = countdownNumber-1;
+            document.getElementById('countdown').innerHTML
+             = countdownNumber;
+
+             // VIDEO KOHTA 22:54 TÄHÄN KOHTAAN JÄITT LOL
+            if (countdownNumber > 3 && countdownNumber <= 6) {
+                // be mammamia
+                document.getElementById('mammamia').className = 'mammamia show';
+            } else {
+                document.getElementById('mammamia').className = 'mammamia';
+            }
+
+            if (countdownNumber == 3) {
+                // veijä käteen
+                document.getElementById('vedäkäteen').className = 'vedäkäteen show';
+            } else {
+                document.getElementById('vedäkäteen').className = 'vedäkäteen';
+            }
+
             if (countdownNumber <= 0) {
                 changeState(3);
             };
